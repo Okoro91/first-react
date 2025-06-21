9a695d034f0c1388ce53daa38f9c6588065256f0
okoro91 <mi.fracri91@gmail.com>
2025-06-21 23:43:13 +0100

    added img | hero section content

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Header from "./component/header/Header";
+import Hero from "./component/hero/Hero";
 
 const App = () => {
   return (
     <div className="container">
       <Header />
+      <Hero />
     </div>
   )
 }
---
+++ src/component/hero/Hero.jsx
@@ -0,0 +1,11 @@
+import Herotext from "../../block/herotext/herotext";
+const Hero = () => {
+    return (
+        <div className="hero">
+            <Herotext />
+            <img src="../../src\assets\image-hero-desktop.png" alt="logo" className="logo"/>
+        </div>
+    )
+}
+
+export default Hero;
